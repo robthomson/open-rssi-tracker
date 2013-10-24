512b2c20464726698402386a3b07ca3c2a375039
rob@marotori.com <rob@marotori.com@a021da7a-eed5-5d49-07a2-d44a5a67e8c1>
2013-10-24 21:53:14 +0000

    Better Filtering of spikes

--- branches/trackerBasic/trackerBasic.ino
+++ branches/trackerBasic/trackerBasic.ino
@@ -23,84 +23,73 @@
 
 #include <Servo.h>
 #include <WProgram.h>
+#include "MovingAvarageFilter.h"
 
 const int inputLeft = A0;     //left  rssi input
 const int inputRight = A1;    //right rssi input
 const int inputMidFront = A3;  //front rssi input
 const int inputMidTop = A2;    //top rssi input
 const int outputServo = 10;    //pin of servo used for pan motion
-const float RSSIsmoothing = 0.0010;  // .0001 = max | 1 = 0ff
+const float smoothing = 5;  //
 
-int counter = 0;
+float rssiLeftAvg = 0;
+float rssiRightAvg = 0;
+float rssiMidFrontAvg = 0;
+float rssiMidTopAvg = 0;
+
 
 const int servocenter = 90;  // absolute center. a 'home made' 360 will no doubt need a suitable value to be found.
 int speed = 0;
 int hasSignal = 0;
-/* 
- these offsets can be used to find center the rssi as most modules are not exact to each other!
- and easy way to find the exact values is to print the rssiLeft & rssiRight values - when the power
- to the rf modules is turned off - well. that worked for me!
- */
-int inputLeftOffset = 17;  
-int inputRightOffset = 0; 
+int offsetLeft = 10;
+int offsetRight = 0;
+
 
 /*
   probably no need to edit below here.
  */
 
 Servo servo;  // create servo object to control a servo
+MovingAvarageFilter rssiLeftAverageFilter(smoothing);
+MovingAvarageFilter rssiRightAverageFilter(smoothing);
+MovingAvarageFilter rssiMidTopAverageFilter(smoothing);
+MovingAvarageFilter rssiMidFrontAverageFilter(smoothing);
 
 
 void setup()
 { 
   Serial.begin(9600);
   servo.attach(outputServo);  
+  
 }
 
 void loop() {
 
+
+ 
   
-  float rssiLeftRough = analogRead(inputLeft) + inputLeftOffset ;
-  float rssiRightRough = analogRead(inputRight) + inputRightOffset ;
-  float rssiMidFrontRough = analogRead(inputMidFront);
-  float rssiMidTopRough = analogRead(inputMidTop);
-
-  int rssiLeft = smooth(rssiLeftRough, RSSIsmoothing, rssiLeft);  
-  int rssiRight = smooth(rssiRightRough, RSSIsmoothing, rssiRight);  
-  int rssiMidFront = smooth(rssiMidFrontRough, RSSIsmoothing, rssiMidFront); 
-  int rssiMidTop = smooth(rssiMidTopRough, RSSIsmoothing, rssiMidTop); 
-
-
-
+  rssiLeftAvg = rssiLeftAverageFilter.process(analogRead(inputLeft) + offsetLeft); 
+  rssiRightAvg = rssiRightAverageFilter.process(analogRead(inputRight) + offsetRight); 
+  rssiMidFrontAvg = rssiMidFrontAverageFilter.process(analogRead(inputMidFront) ); 
+  rssiMidTopAvg = rssiMidTopAverageFilter.process(analogRead(inputMidTop)); 
+  
+  int rssiLeft = rssiLeftAvg;
+  int rssiRight = rssiRightAvg;
+  int rssiMidFront = rssiMidFrontAvg;
+  int rssiMidTop = rssiMidTopAvg;
 
   int raw_rssiDiff = rssiLeft > rssiRight ? rssiLeft - rssiRight : rssiRight - rssiLeft;
   float average_rssi = (rssiLeft + rssiRight ) / 2.f;
-  // Try setting this constant to the average raw rssi value where you are getting good results currently
   const float K_rssi_ratio = 500.f; 
-
   int rssiDiff = (int)( (raw_rssiDiff * K_rssi_ratio) / average_rssi ) ;
-  
-  
-// Serial.println(rssiLeft);
-// Serial.println(rssiRight);
-// Serial.println(rssiMidFront);
-//  Serial.println(rssiMidTop);
-//  Serial.println(" ");
- Serial.println(rssiDiff);
-  Serial.println(" ");
-  /*
-    ----------------------------------------------------------------------------- 
-   TRACKING LOGIC
-   ----------------------------------------------------------------------------- 
-   */
 
+ Serial.println(rssiDiff);
+ Serial.println(" ");
 
   if(rssiMidTop == 0 && rssiMidFront == 0){
     Serial.println("USB");
-    servo.write(servocenter);  
-    delay(2000);
-  }
-  else if ((rssiLeft < 120 || rssiRight <120)){
+    servo.write(servocenter); 
+  } else if ((rssiLeft < 120 || rssiRight <120)){
     if(hasSignal == 0){
       Serial.println("seeking 360");  
       servo.write(servocenter +  20); 
@@ -132,23 +121,7 @@ void loop() {
       servo.write(servocenter);
       delay(2000);
     }  
-
-  }
-  //end
+}
 }
 
-int smooth(int data, float filterVal, float smoothedVal){
-
-
-  if (filterVal > 1){      // check to make sure param's are within range
-    filterVal = .99;
-  }
-  else if (filterVal <= 0){
-    filterVal = 0;
-  }
-
-  smoothedVal = (data * (1 - filterVal)) + (smoothedVal  *  filterVal);
-
-  return (int)smoothedVal;
-}
 
