30b98e4a7425288f410f64394ac16d79d15618a5
rob@marotori.com <rob@marotori.com@a021da7a-eed5-5d49-07a2-d44a5a67e8c1>
2013-11-20 12:58:35 +0000

    Working pan tilt tracker with snap back

--- branches/trackerPanTilt/trackerPanTilt.ino
+++ branches/trackerPanTilt/trackerPanTilt.ino
@@ -36,11 +36,11 @@ int calibrateRight = 0;
 int calibrateUp = 0;
 int calibrateDown = 0;
 int offsetLeft = 0;
-int offsetRight;
-int offsetUp;
-int offsetDown;
+int offsetRight = 30;
+int offsetUp = 14;
+int offsetDown = 0;
 int targetPos;
-int maxspeed = 4;
+int smoothing = 5;
 
 //init servo objects
 Servo servoPan;  
@@ -48,116 +48,154 @@ Servo servoTilt;
 
 //initial servo positions
 int panPos = 90;   //90 = center 0 = right ||  180 = left
-int tiltPos = 180; //180 = straight ahead  || 80 = straight up || 60 = full back
+int tiltPos = 175; //175 = straight ahead  || 80 = straight up || 60 = full back
 
 //max min pos
 int maxTilt = 180;
 int minTilt = 60;
 int maxPan = 180;
 int minPan = 0;
-int snapPoint = 10;
-int panSpeed = 100;
-int tiltSpeed = 100;
+int snapPoint = 2;
+int panSpeed;
+int tiltSpeed;
 int panStep = 1;
 int tiltStep = 2;
 
-//kalman filtering
-double kalman_q= 0.02; //process noise covariance
-double kalman_r= 500; //measurement noise covariance
 
 void setup()
 { 
   Serial.begin(9600);
-  servoPan.attach(servoPanPin);  
-  servoTilt.attach(servoTiltPin); 
-  
- 
-  
-  servoPan.write(panPos);   
-  servoTilt.write(tiltPos);  
-  
-  calibrate();
-
   Serial.println("Init..");
+  servoPan.attach(servoPanPin);  
+  servoTilt.attach(servoTiltPin);   
+  servoPan.write(panPos);   
+  servoTilt.write(tiltPos);   
+  //calibrate();
+  Serial.println("Tracking..");
+
 }
 
 void loop() {
 
-
-
+/*
+  Serial.println(offsetLeft);
+  Serial.println(offsetRight);
+  Serial.println(offsetUp);
+  Serial.println(offsetDown);
+  Serial.println("");
+  */
+  
   rssiLeft = analogRead(inputLeft) + offsetLeft;
   rssiRight = analogRead(inputRight) + offsetRight;
   rssiUp = analogRead(inputUp) + offsetUp;
   rssiDown = analogRead(inputDown) + offsetDown;  
-
-  int rssiPanDiff = rssiLeft > rssiRight ? rssiLeft - rssiRight : rssiRight - rssiLeft;
-  int rssiTiltDiff = rssiUp > rssiDown ? rssiUp - rssiDown : rssiDown - rssiUp;
-
-  if(rssiPanDiff <= 10){
-       panStep = 1; 
-  } else if(rssiPanDiff <= 15){
-      panStep = 2; 
-  } else if(rssiPanDiff <= 20){
-      panStep = 3; 
-  }  else {
-      panStep = 4;    
-  }  
   
-  if(rssiTiltDiff <= 10){
-       tiltStep = 1; 
-  } else if(rssiPanDiff <= 15){
-      tiltStep = 2; 
-  } else if(rssiPanDiff <= 20){
-      tiltStep = 3; 
-  }   else {
-      tiltStep = 4;    
-  }  
+  Serial.println(rssiLeft);
+  Serial.println(rssiRight);
 
+  int raw_rssiPanDiff = rssiLeft > rssiRight ? rssiLeft - rssiRight : rssiRight - rssiLeft;
+  float averagePan_rssi = (rssiLeft + rssiRight ) / 2.f;
+  const float K_rssiPan_ratio = 350.f; 
+  int rssiPanDiff= (int)( (raw_rssiPanDiff * K_rssiPan_ratio) / averagePan_rssi ) ;
+
+  int raw_rssiTiltDiff = rssiUp > rssiDown ? rssiUp - rssiDown : rssiDown - rssiUp;
+  float averageTilt_rssi = (rssiUp + rssiDown ) / 2.f;
+  const float K_rssiTilt_ratio = 350.f; 
+  int rssiTiltDiff= (int)( (raw_rssiTiltDiff * K_rssiTilt_ratio) / averageTilt_rssi ) ;
+  
   panPos = constrain(panPos,minPan,maxPan);
   tiltPos = constrain(tiltPos,minTilt,maxTilt);
 
-  if(rssiPanDiff <= 5){
-    Serial.println("center");
-        servoPan.write(panPos);
-  } else if(rssiLeft >= rssiRight){
-        Serial.println("move right");
-        panPos = panPos + panStep;
-        servoPan.write(panPos);
-        delay(panSpeed);      
-  } else if(rssiLeft <= rssiRight){
-        Serial.println("move left");
-        panPos = panPos - panStep;
-        servoPan.write(panPos);
-        delay(panSpeed);      
-  } 
+  panSpeed = calcspeed(rssiPanDiff);
+  tiltSpeed = calcspeed(rssiTiltDiff);
+
+   if(rssiLeft < 130 && rssiRight < 130){
+        //no lock do a sweep
+        int pos = 0;
+               for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+                {                                  // in steps of 1 degree 
+                  servoPan.write(pos);              // tell servo to go to position in variable 'pos' 
+                  delay(20);                       // waits 15ms for the servo to reach the position 
+                  
+                  if(rssiLeft >= 130 || rssiRight >= 130){
+                     break; 
+                  }  
+                  
+                } 
+                for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+                {                                
+                  servoPan.write(pos);              // tell servo to go to position in variable 'pos' 
+                  delay(20);                       // waits 15ms for the servo to reach the position 
+                  
+                  if(rssiLeft >= 130 || rssiRight >= 130){
+                     break; 
+                  }
+                  
+                }         
+     
+      
+   }  else {
+
+ 
+        //left right
+        if(rssiPanDiff <= smoothing){
+          Serial.println("center");
+              servoPan.write(panPos);
+        } else if(rssiLeft >= rssiRight){
+              Serial.println("move right");
+              panPos = panPos + panStep;
+              servoPan.write(panPos);
+              delay(panSpeed);      
+        } else if(rssiLeft <= rssiRight){
+              Serial.println("move left");
+              panPos = panPos - panStep;
+              servoPan.write(panPos);
+              delay(panSpeed);      
+        } 
+      
+        //up down
+        if(rssiTiltDiff <= smoothing){
+              Serial.println("center");
+              servoTilt.write(tiltPos);
+        } else if(rssiUp >= rssiDown){
+              Serial.println("move up");
+              tiltPos = tiltPos + tiltStep;
+              servoTilt.write(tiltPos);
+              delay(tiltSpeed);      
+        } else if(rssiUp <= rssiDown){
+              Serial.println("move down");
+              tiltPos = tiltPos - tiltStep;
+              servoTilt.write(tiltPos);
+              delay(tiltSpeed);      
+        } 
 
 
-
-  if(rssiTiltDiff <= 10){
-        Serial.println("center");
-        servoTilt.write(tiltPos);
-  } else if(rssiUp >= rssiDown){
-        Serial.println("move up");
-        tiltPos = tiltPos + tiltStep;
-        servoTilt.write(tiltPos);
-        delay(tiltSpeed);      
-  } else if(rssiUp <= rssiDown){
-        Serial.println("move down");
-        tiltPos = tiltPos - tiltStep;
-        servoTilt.write(tiltPos);
-        delay(tiltSpeed);      
-  } 
-  
-
-
-
+      //snap
+        if(panPos <= minPan + 5 ){
+            if(rssiRight > rssiLeft && rssiPanDiff >= snapPoint){
+                 panPos = 160;                              
+                 servoPan.write(panPos);              // tell servo to go to position in variable 'pos' 
+                 delay(2000);                       // waits 15ms for the servo to reach the position         
+            }    
+        }  
+        if(panPos >= maxPan - 5 ){
+          if(rssiRight < rssiLeft && rssiPanDiff >= snapPoint){
+                 panPos = 20;                              
+                 servoPan.write(panPos);              // tell servo to go to position in variable 'pos' 
+                 delay(2000);                       // waits 15ms for the servo to reach the position  
+          }      
+        }    
+        
+   }
   
 
 }
 
+
 void calibrate(){
 
   Serial.println("Calibrate..");
+  delay(2000);
   int i;
   for(i=0;i<10;i++)
   {
@@ -199,6 +237,30 @@ void calibrate(){
 }  
 
 
+int calcspeed(int rssiDiff){
+  int speed;
+  if(rssiDiff <= smoothing){
+      speed = 0;
+    } 
+    else if (rssiDiff <= smoothing + 1){
+      speed = 200 ;
+    } 
+    else if (rssiDiff <= smoothing + 2){
+      speed = 150 ;
+    } 
+    else if (rssiDiff <= smoothing + 4){
+      speed =  100;
+    } 
+    else if (rssiDiff <= smoothing + 6){
+      speed = 50 ;
+    } 
+    else if (rssiDiff <= smoothing + 8){
+      speed = 30 ;         
+    } else {
+      speed = 10; 
+    } 
+   return speed; 
+}  
 
 
 
